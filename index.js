990ca610c6a58bb61b9f1d14cdabbd613c69bc71
HParmeshwar <85221753+HimanshuParmeshwar@users.noreply.github.com>
2022-07-01 22:32:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+var i;
+for(i = 0; i<7; i++){
+  document.querySelectorAll(".set button")[i].addEventListener("click", Uponcalling);
+}
+
+
+function Uponcalling(){
+  var buttoninnerhtml = this.innerHTML;
+  switch (buttoninnerhtml) {
+    case "w":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+
+    case "a":
+      var audio1 = new Audio("sounds/kick-bass.mp3");
+      audio1.play();
+      break;
+    case "s":
+      var audio2 = new Audio("sounds/tom-4.mp3");
+      audio2.play();
+      break;
+    case "d":
+      var audio3 = new Audio("sounds/snare.mp3");
+      audio3.play();
+      break;
+    case "j":
+      var audio4 = new Audio("sounds/tom-1.mp3");
+      audio4.play();
+      break;
+    case "k":
+      var audio5 = new Audio("sounds/tom-2.mp3");
+      audio5.play();
+      break;
+    case "l":
+      var audio6 = new Audio("sounds/tom-3.mp3");
+      audio6.play();
+      break;
+  }
+}
